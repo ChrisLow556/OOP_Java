318ffc0bbdaf0e871ea8d82722191bf8d139af93
ChrisLow556 <chrislowxiaohiang@gmail.com>
2025-04-15 15:30:48 +0800

    New files to test in playground folder

---
+++ oop_test1/src/playground/testClock.java
@@ -0,0 +1,10 @@
+package playground;
+
+import java.util.Date;
+
+public class testClock {
+    public static void main(String[] args) {
+        Date time = new Date();
+        System.out.println(time);
+    }
+}
