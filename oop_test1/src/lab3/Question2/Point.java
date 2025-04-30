c3eeff58169c83af6fdc9e5a05621b7fb8ef8113
ChrisLow556 <chrislowxiaohiang@gmail.com>
2025-04-30 11:21:53 +0800

    Another hotfix cuz i fucked up

--- oop_test1/src/lab3/Question2/Point.java
+++ oop_test1/src/lab3/Question2/Point.java
@@ -15,4 +15,17 @@ public class Point {
     public double getY() {
         return y;
     }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setPoint(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
 }
